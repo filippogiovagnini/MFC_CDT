0a901bf1e05e756aa84c1ce330d40a8a703c58e8
Filippo Giovagnini <filippo.giovagnini@gmail.com>
2024-10-12 15:50:38 +0100

    change

--- first_lecture_numerics.py
+++ first_lecture_numerics.py
@@ -2,11 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 u = 1.0
-nx = 40
+nx = 80
 x = np.linspace(0.0, 1.0, nx + 1)
-nt = 80
-dx = 1./nx
+nt = 40
+dx = 2./nx
 dt = 1./nt
+
+# \mu = u \frac{\Delta t}{\Delta x}
+# If \mu > 1, the solution is unstable. If \mu < 1, the solution is stable.
+
 # The spacial resolution
 # The time step
 # The initial conditions and arrays for the old and new time steps
@@ -24,7 +28,7 @@ plt. pause(1)
 for n in range(nt):
   for j in range(1,nx): # loop over space from 1 to nx-1
 # (avoiding boundary conditions)
-    phi[j] = phiOld[j] - u*dt*(phiOld[j + 1] - phiOld[j])/dx
+    phi[j] = phiOld[j] - u*dt*(phiOld[j] - phiOld[j - 1])/dx
   phiOld = phi.copy()
   plt.cla()
   plt.plot(x, phi, 'b', label = 'Time_' + str(n*dt))
